46cb1a24b8174912696659d1c2522df2655c02a6
Dragomir Andrei <andrei.dragomir1401@stud.acs.upb.ro>
2023-05-19 23:56:04 +0300

    Update ferate.cpp

--- ferate.cpp
+++ ferate.cpp
@@ -5,164 +5,166 @@
 #include <vector>
 using namespace std;
 
-class Task
-{
-public:
-    void solve()
-    {
-        read_input();
-        print_output(get_result());
-    }
+class Task {
+	public:
+		void solve() {
+			read_input();
+			print_output(get_result());
+		}
 
-private:
-    static constexpr int NMAX = (int)1e5 + 5;
-    int nr_stations, nr_paths, source;
-    vector<int> adj[NMAX];                           // adjancency list
-    vector<int> adj_condense[NMAX];                  // adjancency list for condensed graph
-    unordered_map<int, int> scc_containing_node_map; // map node to scc containing it
-    vector<int> parent;                              // parent[i] = parent of node i in dfs tree
-    vector<bool> vis;                                // vis[i] = true if node i was visited
-    vector<int> low_link;                            // low_link[i] = lowest timestamp of node i
-    stack<int> in_stack;                             // stack used in tarjan
-    vector<vector<int>> all_sccs;                    // all sccs in the graph
-    int timestamp = 0;                               // timestamp used in tarjan
+	private:
+		static constexpr int NMAX = (int)1e5 + 5;
+		int nr_stations, nr_paths, source;
+		// adjancency list
+		vector<int> adj[NMAX];
+		// adjancency list for condensed graph
+		vector<int> adj_condense[NMAX];
+		// map node to scc containing it
+		unordered_map<int, int> scc_containing_node_map;
+		// parent[i] = parent of node i in dfs tree
+		vector<int> parent;
+		// vis[i] = true if node i was visited
+		vector<bool> vis;
+		// low_link[i] = lowest timestamp of node i
+		vector<int> low_link;
+		// stack used in tarjan
+		stack<int> in_stack;
+		// all sccs in the graph
+		vector<vector<int>> all_sccs;
+		// timestamp used in tarjan
+		int timestamp = 0;
 
-    void read_input()
-    {
-        ifstream fin("ferate.in");
-        fin >> nr_stations >> nr_paths >> source;
-        for (int i = 1, x, y; i <= nr_paths; i++)
-        {
-            fin >> x >> y;
-            adj[x].push_back(y);
-        }
-        fin.close();
-    }
+		void read_input() {
+			ifstream fin("ferate.in");
+			fin >> nr_stations >> nr_paths >> source;
+			for (int i = 1, x, y; i <= nr_paths; i++) {
+				fin >> x >> y;
+				adj[x].push_back(y);
+			}
+			fin.close();
+		}
 
-    void tarjan(int node, vector<vector<int>> &all_sccs)
-    {
-        // Tarjan algorithm for finding all sccs
-        parent[node] = low_link[node] = ++timestamp; // set parent and low_link
-        in_stack.push(node);                         // push node in stack
-        vis[node] = true;                            // mark node as visited
+		void tarjan(int node, vector<vector<int>> &all_sccs) {
+			// Tarjan algorithm for finding all sccs
 
-        for (long unsigned int i = 0; i < adj[node].size(); i++) // for each neighbour
-        {
-            if (parent[adj[node][i]] == 0) // if parent is not set
-            {
-                tarjan(adj[node][i], all_sccs);                               // dfs
-                low_link[node] = min(low_link[node], low_link[adj[node][i]]); // update low_link
-            }
-            else if (vis[adj[node][i]] == true) // if neighbour is visited
-            {
-                low_link[node] = min(low_link[node], low_link[adj[node][i]]); // update low_link
-            }
-        }
+			// set parent and low_link
+			parent[node] = low_link[node] = ++timestamp;
+			// push node in stack
+			in_stack.push(node);
+			// mark node as visited
+			vis[node] = true;
 
-        if (low_link[node] == parent[node]) // if node is the root of a scc
-        {
-            vector<int> scc;
-            int aux;
+			// for each neighbour
+			for (long unsigned int i = 0; i < adj[node].size(); i++) {
+				// if parent is not set
+				if (parent[adj[node][i]] == 0) {
+					tarjan(adj[node][i], all_sccs);
+					// update low_link
+					low_link[node] = min(low_link[node],
+										 low_link[adj[node][i]]);
+				} else if (vis[adj[node][i]] == true) {
+				 	// update low_link
+					low_link[node] = min(low_link[node],
+										 low_link[adj[node][i]]);
+				}
+			}
 
-            // Pop nodes from stack until node is found
-            do
-            {
-                // Pop node from stack
-                aux = in_stack.top();
-                in_stack.pop();
+			// if node is the root of a scc
+			if (low_link[node] == parent[node]) {
+				vector<int> scc;
+				int aux;
 
-                // Add node to scc
-                scc.push_back(aux);
+				// Pop nodes from stack until node is found
+				do {
+					// Pop node from stack
+					aux = in_stack.top();
+					in_stack.pop();
 
-                // Update scc_containing_node_map
-                scc_containing_node_map[aux] = all_sccs.size() + 1;
+					// Add node to scc
+					scc.push_back(aux);
 
-                // Mark node as not visited
-                vis[aux] = false;
-            } while (aux != node);
+					// Update scc_containing_node_map
+					scc_containing_node_map[aux] = all_sccs.size() + 1;
 
-            // Add scc to all_sccs
-            all_sccs.push_back(scc);
-        }
-    }
+					// Mark node as not visited
+					vis[aux] = false;
+				} while (aux != node);
 
-    int find_number_of_adding_edges()
-    {
-        // Build condensed graph
-        vector<int> in_degree(all_sccs.size() + 1, 0);
+				// Add scc to all_sccs
+				all_sccs.push_back(scc);
+			}
+		}
 
-        for (int i = 1; i <= nr_stations; i++) // for each node
-        {
-            // Get scc containing node
-            int scc = scc_containing_node_map[i];
+		int find_number_of_adding_edges() {
+			// Build condensed graph
+			vector<int> in_degree(all_sccs.size() + 1, 0);
 
-            for (int j = 0; j < adj[i].size(); j++) // for each neighbour
-            {
-                // Get scc containing neighbour
-                int scc_neigh = scc_containing_node_map[adj[i][j]];
+			// For each node
+			for (int i = 1; i <= nr_stations; i++) {
+				// Get scc containing node
+				int scc = scc_containing_node_map[i];
 
-                // If the neighbour is not in the same scc
-                if (scc != scc_neigh)
-                {
-                    // It means that there is an edge between scc and scc_neigh
-                    in_degree[scc_neigh]++;
-                }
-            }
-        }
+				// For each neigh
+				for (long unsigned int j = 0; j < adj[i].size(); j++) {
+					// Get scc containing neighbour
+					int scc_neigh = scc_containing_node_map[adj[i][j]];
 
-        int nr_adding_edges = 0;
+					// If the neighbour is not in the same scc
+					if (scc != scc_neigh) {
+						// It means that there is an edge
+						// between scc and scc_neigh
+						in_degree[scc_neigh]++;
+					}
+				}
+			}
 
-        // // Get scc containing source
-        int scc_src = scc_containing_node_map[source];
+			int nr_adding_edges = 0;
 
-        // Count the number of sccs that have in_degree 0
-        for (int i = 1; i <= all_sccs.size(); i++)
-        {
-            // If the scc is not the scc containing source and it has in_degree 0
-            if (in_degree[i] == 0 && i != scc_src)
-            {
-                nr_adding_edges++;
-            }
-        }
+			// Get scc containing source
+			long unsigned int scc_src = scc_containing_node_map[source];
 
-        return nr_adding_edges;
-    }
+			// Count the number of sccs that have in_degree 0
+			for (long unsigned int i = 1; i <= all_sccs.size(); i++) {
+				// If the scc is not the scc containing source
+				// and it has in_degree 0
+				if (in_degree[i] == 0 && i != scc_src) {
+					nr_adding_edges++;
+				}
+			}
 
-    int get_result()
-    {
-        parent = vector<int>(nr_stations + 1, 0);
-        vis = vector<bool>(nr_stations + 1, false);
-        low_link = vector<int>(nr_stations + 1, 0);
+			return nr_adding_edges;
+		}
 
-        for (int node = 1; node <= nr_stations; node++) // for each node
-        {
-            if (parent[node] == 0) // if parent is not set
-            {
-                tarjan(node, all_sccs); // tarjan
-            }
-        }
+		int get_result() {
+			parent = vector<int>(nr_stations + 1, 0);
+			vis = vector<bool>(nr_stations + 1, false);
+			low_link = vector<int>(nr_stations + 1, 0);
 
-        return find_number_of_adding_edges();
-    }
+			for (int node = 1; node <= nr_stations; node++) {
+				// If parent is not set
+				if (parent[node] == 0) {
+					tarjan(node, all_sccs);
+				}
+			}
 
-    void
-    print_output(int result)
-    {
-        ofstream fout("ferate.out");
-        fout << result << '\n';
-        fout.close();
-    }
-};
+			return find_number_of_adding_edges();
+		}
 
-int main()
-{
-    auto *task = new (nothrow) Task();
-    if (!task)
-    {
-        cerr << "new failed: WTF are you doing? Throw your PC!\n";
-        return -1;
-    }
-    task->solve();
-    delete task;
-    return 0;
-}
+		void
+		print_output(int result) {
+			ofstream fout("ferate.out");
+			fout << result << '\n';
+			fout.close();
+		}
+	};
+
+	int main() {
+		auto *task = new (nothrow) Task();
+		if (!task) {
+			cerr << "new failed: WTF are you doing? Throw your PC!\n";
+			return -1;
+		}
+		task->solve();
+		delete task;
+		return 0;
+	}
